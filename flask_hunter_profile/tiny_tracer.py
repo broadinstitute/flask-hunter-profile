ca9314da182fa482919d67b58355ef00c563b416
Philip Montgomery <pmontgom@broadinstitute.org>
2024-01-04 10:07:11 -0500

    force tracer to use python Tracer class and avoid cython one

--- flask_hunter_profile/tiny_tracer.py
+++ flask_hunter_profile/tiny_tracer.py
@@ -1,5 +1,9 @@
 import hunter
+import hunter.tracer
 from hunter import Event
+import traceback
+import re
+from typing import Callable
 import traceback
 
 class ContinueTraceOption:
@@ -11,36 +15,9 @@ class ContinueTraceOption:
 TRACE_CHILD = ContinueTraceOption("TRACE_CHILD")
 NO_TRACE = ContinueTraceOption("NO_TRACE")
 
-from typing import Callable
-import sys
-import threading
-import traceback
-class Tracer(hunter.Tracer):
-
-    # def trace(self, predicate):
-    #     """
-    #     Starts tracing with the given callable.
-
-    #     Args:
-    #         predicate (callable that accepts a single :obj:`~hunter.event.Event` argument):
-    #     Return:
-    #         self
-    #     """
-    #     self._handler = predicate
-    #     if self.profiling_mode:
-    #         if self.threading_support is None or self.threading_support:
-    #             self._threading_previous = getattr(threading, '_profile_hook', None)
-    #             threading.setprofile(self)
-    #         self._previous = sys.getprofile()
-    #         sys.setprofile(self)
-    #     else:
-    #         if self.threading_support is None or self.threading_support:
-    #             self._threading_previous = getattr(threading, '_trace_hook', None)
-    #             threading.settrace(self)
-    #         self._previous = sys.gettrace()
-    #         sys.settrace(self)
-    #     return self
-
+class Tracer(hunter.tracer.Tracer):
+    # need to override the __call__ method on tracer in order to change behavior of handler.
+    # at this point, would it make more sense to copy the whole class and fork the implementation?
 
     def __call__(self, frame, kind, arg):
         # don't both tracing individual lines
@@ -62,26 +39,6 @@ class Tracer(hunter.Tracer):
                 return None
 
 
-from hunter.const import SITE_PACKAGES_PATHS, SYS_PREFIX_PATHS
-
-# def is_stdlib(module, filename):
-#     module_parts = module.split('.')
-#     if 'pkg_resources' in module_parts:
-#         # skip this over-vendored module
-#         return True
-#     elif (module.startswith('namedtuple_') or module == 'site'):
-#         # skip namedtuple exec garbage
-#         return True
-#     elif filename.startswith(SITE_PACKAGES_PATHS):
-#         # if it's in site-packages then its definitely not stdlib
-#         return False
-#     elif filename.startswith(SYS_PREFIX_PATHS):
-#         return True
-#     else:
-#         return False
-
-import re
-
 class Handler():
     def __init__(self, trace_module_patterns=[], skip_action_modules=[], action=lambda event: None):
         self.trace_option_by_module = {}
@@ -91,9 +48,6 @@ class Handler():
         self.seen_modules = set()
 
     def __call__(self, event: hunter.Event): 
-        # print("handler.__call__", event.kind, event.function, event.module)
-        # module = event.frame.f_globals.get('__name__', '?')
-
         # check the caller's module to decide whether we want to trace into function
         module = event.frame.f_back.f_globals.get('__name__', '?')
 
